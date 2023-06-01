285ea503cebe343ba02f6663c1feaad14c2ad24b
xiaolaba <xiao_laba_cn@yahoo.com>
2023-06-01 12:00:39 +0800

    Create build_README_md.bat

---
+++ build_README_md.bat
@@ -0,0 +1,28 @@
+set last_edit=2014-06-22
+set myname=XIAOLABA
+set myemail=xiao_laba_cn@yahoo.com
+set ccemail=xiao_laba_cn@yahoo.com
+
+
+
+set OUTPUT_HTML=README.html
+set OUTPUT_GITHUB_README=README.md
+
+setlocal enabledelayedexpansion
+
+
+
+echo # AVR_ISP_PIN   > %OUTPUT_GITHUB_README%
+echo AVR MCU, ISP pin# table, easy design aid   >> %OUTPUT_GITHUB_README%
+echo by xiaolaba, last edit %last_edit%   >> %OUTPUT_GITHUB_README%
+for /R ./PNG/ %%G in (*.PNG) do (
+	set "FILENAME=%%~nG"  REM default extension change to .bdf
+	::set "EXT_BDF=PNG"  REM default extension change to .png
+	::set "BOOKY_EXT=txt"  REM default extension change to .txt
+    ::echo ^<li^>^<a href="http://link-to-file.com/!relfolder!/!FILENAME!%%~xb"^>%%~nb^</a^>^</li^>
+	:::::: this file name only, removed local path string
+    echo ^^![ PNG/%FILENAME%%%~nG%%~xG ]^( PNG/%FILENAME%%%~nG%%~xG ^)  
+) >> %OUTPUT_GITHUB_README%
+
+
+REM goto exit
