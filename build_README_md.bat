60a2fbd35396646d2c744fa8fa8745eadbb6ef57
xiaolaba <xiao_laba_cn@yahoo.com>
2023-06-01 12:12:44 +0800

    Update build_README_md.bat

--- build_README_md.bat
+++ build_README_md.bat
@@ -15,13 +15,19 @@ setlocal enabledelayedexpansion
 echo # AVR_ISP_PIN   > %OUTPUT_GITHUB_README%
 echo AVR MCU, ISP pin# table, easy design aid   >> %OUTPUT_GITHUB_README%
 echo by xiaolaba, last edit %last_edit%   >> %OUTPUT_GITHUB_README%
+echo.     >> %OUTPUT_GITHUB_README%
+echo.     >> %OUTPUT_GITHUB_README%
+
 for /R ./PNG/ %%G in (*.PNG) do (
 	set "FILENAME=%%~nG"  REM default extension change to .bdf
 	::set "EXT_BDF=PNG"  REM default extension change to .png
 	::set "BOOKY_EXT=txt"  REM default extension change to .txt
     ::echo ^<li^>^<a href="http://link-to-file.com/!relfolder!/!FILENAME!%%~xb"^>%%~nb^</a^>^</li^>
 	:::::: this file name only, removed local path string
-    echo ^^![ PNG/%FILENAME%%%~nG%%~xG ]^( PNG/%FILENAME%%%~nG%%~xG ^)  
+    echo [ %FILENAME%%%~nG%%~xG ]^( PNG/%FILENAME%%%~nG%%~xG ^)  
+    echo ^^![ PNG/%FILENAME%%%~nG%%~xG ]^( PNG/%FILENAME%%%~nG%%~xG ^)   
+	:: blank line, 2 spaces, github line break
+	echo.    
 ) >> %OUTPUT_GITHUB_README%
 
 
